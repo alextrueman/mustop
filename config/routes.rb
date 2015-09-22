73ec2f3eecefb322ea155afb3c51462f003362c1
Alex <alexandr1golubenko@gmail.com>
2015-09-22 12:26:40 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :top_musics
- 
+  root "top_musics#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
